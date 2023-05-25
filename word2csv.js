72a1293516270f67d7ef5ab25d854c96d4a8ab88
vincent scotto <vincent@scotto.ws>
2023-05-25 10:41:09 -0700

    removing trailing line break at the end of the cs

--- word2csv.js
+++ word2csv.js
@@ -1,93 +1,97 @@
-const fs = require('fs');
-const mammoth = require('mammoth');
-const csvWriter = require('csv-writer');
+const fs = require("fs");
+const mammoth = require("mammoth");
+const csvWriter = require("csv-writer");
 
 function convertWordToCSV(inputPath, outputPath) {
-  return new Promise((resolve, reject) => {
-    const options = {
-      styleMap: [
-        'p[style-name="Eyebrow copy"] => Eyebrow copy',
-        'p[style-name="Body copy 1"] => Body copy 1',
-        'p[style-name="Body copy 2"] => Body copy 2',
-        'p[style-name="Footnote copy"] => Footnote copy',
-      ],
-    };
+	return new Promise((resolve, reject) => {
+		const options = {
+			styleMap: [
+				'p[style-name="Eyebrow copy"] => Eyebrow copy',
+				'p[style-name="Body copy 1"] => Body copy 1',
+				'p[style-name="Body copy 2"] => Body copy 2',
+				'p[style-name="Footnote copy"] => Footnote copy'
+			]
+		};
 
-    mammoth
-      .extractRawText({ path: inputPath }, options)
-      .then((result) => {
-        const paragraphs = result.value.split('\n');
-        const frames = [];
+		mammoth
+			.extractRawText({ path: inputPath }, options)
+			.then(result => {
+				const paragraphs = result.value.split("\n");
+				const frames = [];
 
-        let frame = {};
-        let frameIndex = 1;
+				let frame = {};
+				let frameIndex = 1;
 
-        let currentHeader = '';
+				let currentHeader = "";
 
-        for (const paragraph of paragraphs) {
-          if (paragraph.startsWith('Frame')) {
-            if (Object.keys(frame).length > 0) {
-              frames.push(frame);
-              frame = {};
-            }
-            frame['ID'] = `Frame ${frameIndex++}`;
-          } else if (paragraph !== '=== FRAME END ===') {
-            if (paragraph.includes(': ')) {
-              const separatorIndex = paragraph.indexOf(': ');
-              const key = paragraph.substring(0, separatorIndex);
-              const value = paragraph.substring(separatorIndex + 2).trim();
-              frame[key] = value;
-              currentHeader = key;
-            } else if (currentHeader) {
-              frame[currentHeader] += ' ' + paragraph.trim();
-            }
-          }
-        }
+				for (const paragraph of paragraphs) {
+					if (paragraph.startsWith("Frame")) {
+						if (Object.keys(frame).length > 0) {
+							frames.push(frame);
+							frame = {};
+						}
+						frame["ID"] = `Frame ${frameIndex++}`;
+					} else if (paragraph !== "=== FRAME END ===") {
+						if (paragraph.includes(": ")) {
+							const separatorIndex = paragraph.indexOf(": ");
+							const key = paragraph.substring(0, separatorIndex);
+							const value = paragraph.substring(separatorIndex + 2).trim();
+							frame[key] = value;
+							currentHeader = key;
+						} else if (currentHeader) {
+							frame[currentHeader] += " " + paragraph.trim();
+						}
+					}
+				}
 
-        if (Object.keys(frame).length > 0) {
-          frames.push(frame);
-        }
+				if (Object.keys(frame).length > 0) {
+					frames.push(frame);
+				}
 
-        const csvHeader = [
-          { id: 'ID', title: 'ID' },
-          { id: 'Eyebrow copy', title: 'Eyebrow copy' },
-          { id: 'Body copy 1', title: 'Body copy 1' },
-          { id: 'Body copy 2', title: 'Body copy 2' },
-          { id: 'Footnote copy', title: 'Footnote copy' },
-        ];
+				const csvHeader = [
+					{ id: "ID", title: "ID" },
+					{ id: "Eyebrow copy", title: "Eyebrow copy" },
+					{ id: "Body copy 1", title: "Body copy 1" },
+					{ id: "Body copy 2", title: "Body copy 2" },
+					{ id: "Footnote copy", title: "Footnote copy" }
+				];
 
-        const csvWriterOptions = {
-          header: csvHeader,
-          path: outputPath,
-          fieldDelimiter: ';',
-        };
+				const csvWriterOptions = {
+					header: csvHeader,
+					path: outputPath,
+					fieldDelimiter: ";"
+				};
 
-        const csvData = frames.map((frame) => ({
-          ID: frame['ID'] || '',
-          'Eyebrow copy': (frame['Eyebrow copy'] || '').trim(),
-          'Body copy 1': (frame['Body copy 1'] || '').trim(),
-          'Body copy 2': (frame['Body copy 2'] || '').trim(),
-          'Footnote copy': (frame['Footnote copy'] || '').trim(),
-        }));
+				const csvData = frames.map(frame => ({
+					ID: frame["ID"] || "",
+					"Eyebrow copy": (frame["Eyebrow copy"] || "").trim(),
+					"Body copy 1": (frame["Body copy 1"] || "").trim(),
+					"Body copy 2": (frame["Body copy 2"] || "").trim(),
+					"Footnote copy": (frame["Footnote copy"] || "").trim()
+				}));
 
-        const writer = csvWriter.createObjectCsvWriter(csvWriterOptions);
+				const writer = csvWriter.createObjectCsvWriter(csvWriterOptions);
 
-        writer
-          .writeRecords(csvData)
-          .then(() => {
-            console.log('\nCSV file has been written successfully.');
-            resolve(outputPath);
-          })
-          .catch((error) => {
-            console.error('Error writing CSV file:', error);
-            reject(error);
-          });
-      })
-      .catch((error) => {
-        console.error('Error extracting text from the Word document:', error);
-        reject(error);
-      });
-  });
+				writer
+					.writeRecords(csvData)
+					.then(() => {
+						const csvContent = fs
+							.readFileSync(outputPath, "utf-8")
+							.replace(/\r?\n$/, "");
+						fs.writeFileSync(outputPath, csvContent);
+						console.log("\nCSV file has been written successfully.");
+						resolve(outputPath);
+					})
+					.catch(error => {
+						console.error("Error writing CSV file:", error);
+						reject(error);
+					});
+			})
+			.catch(error => {
+				console.error("Error extracting text from the Word document:", error);
+				reject(error);
+			});
+	});
 }
 
 module.exports = convertWordToCSV;
